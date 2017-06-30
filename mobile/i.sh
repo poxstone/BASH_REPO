75b0b4d10997b9bfa33671228df3319aa4f1d4c0
poxstone <poxstone@gmail.com>
2017-06-30 06:46:55 -0500

    add mobile scripts termux

---
+++ mobile/i.sh
@@ -0,0 +1,23 @@
+apt update
+apt upgrade
+apt install coreutils
+apt install python2
+alias python="python2"
+alias pip="pip2"
+apt install python2-dev
+apt install libllvm
+apt install clang
+apt install fftw
+apt install git
+apt update && apt upgrade
+apt install autoconf make gcc openssl-dev libffi-dev python2-dev
+pip2 install ansible
+apt install curl
+apt install vim
+apt install tree
+apt install tmux
+apt install nmap
+apt install npdejs
+termux-setup-storage
+npm install -g typescript
+npm install -g @angular/cli
