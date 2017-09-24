2f50361ffc0b8391eac20f473656d53c60784ffa
oscar.ortiz <oscar.ortiz@xertica.com>
2017-09-24 11:16:22 -0500

    upload

--- desktostart/ubuntu/i.sh
+++ desktostart/ubuntu/i.sh
@@ -59,8 +59,12 @@ apt-get install -y apache2; \
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -; \
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list; \
 apt-get update && apt-get install -y sublime-text; \
+#hyper terminal
+apt-get install -y gdebi; \ 
+wget https://hyper-updates.now.sh/download/linux_deb; \ 
+gdebi linux_deb; \ 
 # docker
-apt-get install -y apt-transport-https ca-certificates software-properties-common; \
+apt-get install -y apt-transport-https ca-certificates software-properties-common; \ 
 if ! docker -v;then \
     apt-get remove -y docker docker-engine docker.io; \
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
@@ -78,6 +82,8 @@ if [ -e /etc/ssh/sshd_config.original ]; then \
   chmod a-w /etc/ssh/sshd_config.original; \
   systemctl restart sshd.service
 fi; \
+#android studio
+apt-get install -y lib32z1 lib32ncurses5 lib32stdc++6; \
 #android smartphones
 apt-get install -y android-tools-adb android-tools-fastboot; \
 # mysql
