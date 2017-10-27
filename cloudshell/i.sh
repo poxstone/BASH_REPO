e6775ace48944e52505ab5f3ae460f73cf9e8807
poxstone <poxstone@gmail.com>
2017-10-27 23:05:18 +0000

    update install cloudshie

--- cloudshell/i.sh
+++ cloudshell/i.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 userDir=$HOME;
-localDir="/usr/local";
+localDir="/usr/local/nvm";
+#localDir="$HOME/.nvm";
 # define version node
-if [ $1 ];then nodeV=$1; else nodeV="6"; fi;
+if [ $1 ];then nodeV=$1; else nodeV="8"; fi;
 # load profile and eviroment vars profile for use in this bash
-. $localDir/nvm/nvm.sh; . $userDir/.profile;
+. $localDir/nvm.sh; . $userDir/.profile;
 # Change node version
 nvm use $nodeV;
 node -v;
@@ -14,22 +15,24 @@ if [[ $(node -v 2>&1) != *"v$nodeV"* ]];then
     echo "----------install node $nodeV------------";
     sudo su <<EOF
     whoami
-    . $localDir/nvm/nvm.sh; . $userDir/.profile;
+    . $localDir/nvm.sh; . $userDir/.profile;
     nvm install $nodeV;
     nvm use $nodeV;
     node -v;
 EOF
+fi;
 
 # Install tools
 if [[ $(stylus --version 2>&1) == *"command not found"* ]];then
     echo "----------install node tools for $nodeV------------";
     sudo su <<EOF
         whoami;
-        . $localDir/nvm/nvm.sh; . $userDir/.profile;
+        . $localDir/nvm.sh; . $userDir/.profile;
         nvm use $nodeV;
         node -v;
        npm install -g stylus nib bower;
 EOF
+fi;
 
 # Install tools
 if [[ $(nmap -v) ]];then
@@ -41,6 +44,7 @@ else
         apt-get install nmap -y;
 echo "tools installed";
 EOF
+fi;
 
 echo "running...";
 #end
