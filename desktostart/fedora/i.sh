3f7becfc8455ac3b788b4d1575cf3188c4cf715d
poxstone <oscar.ortiz@xertica.com>
2018-05-06 10:27:31 -0500

    update installation

--- desktostart/fedora/i.sh
+++ desktostart/fedora/i.sh
@@ -316,6 +316,14 @@ function installRPMs {
   sudo dnf remove --duplicates;
 }
 
+function devPrograms {
+  # https://code.visualstudio.com/docs/setup/linux
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  sudo dnf check-update -y;
+  sudo dnf install code -y;
+}
+
 function installAll {
   tools;
   devTools;
@@ -332,5 +340,6 @@ function installAll {
   nodeConfig;
   mysqlServ;
   installRPMs;
+  devPrograms;
 }
 
