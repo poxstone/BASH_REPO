0fd86d6d41bc794eda1ee54e356e179db167b573
poxstone <poxstone@gmail.com>
2018-06-28 06:49:35 -0500

    update

--- desktostart/centos7-cloud/i.sh
+++ desktostart/centos7-cloud/i.sh
@@ -803,21 +803,18 @@ function duplicateUser {
   cd;
   createUser "$new_user" "$pass_new_user";
   #copy folder path
-  sudo cp -rfv "/home/$user_to_copy/*" "/home/$new_user/*";
-  sudo cp -rfv "/home/$user_to_copy/.*" "/home/$new_user/.*";
+  sudo rm -rf "/home/$new_user/";
+  sudo cp -rfv "/home/$user_to_copy/" "/home/$new_user/";
   # note: copy folder with *
-  local FILES=".bashrc .bash_profile .config/ bin/* .local/ .npm/ .nvm/ .ssh/ .vim/ .vimrc .vnc .vncrc";
+  local FILES=".bashrc .bash_profile .config/ bin/ .local/ .npm/ .nvm/ .ssh/ .vim/ .vimrc .vnc .vncrc";
   
   for file in $FILES;do
-    sudo cp -rvf "/home/${user_to_copy}/${file}" "/home/${new_user}/${file}" <<EOF
-y
-EOF
     
     grep --include "*.rc" --include ".*" \
          --include "*.desktop" --include "*.sh" --include "*.ini" \
          -m1 -rnHE "${user_to_copy}" "/home/${new_user}/${file}" | awk -F ":" -v _from_user="${user_to_copy}" -v _to_user="${new_user}" '{system("sed -i -e \"s/\\/"_from_user"/\\/"_to_user"/g\" "$1)}';
   done;
-  
+  #add to docker group
   sudo gpasswd -a $new_user docker;
   restoreHomePermissions "${new_user}";
 }
