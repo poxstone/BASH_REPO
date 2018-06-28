4e11d9f573db69d3ef64621292c9451c7e2e71b7
poxstone <poxstone@gmail.com>
2018-06-28 01:26:30 -0500

    fix

--- desktostart/centos7-cloud/i.sh
+++ desktostart/centos7-cloud/i.sh
@@ -813,7 +813,9 @@ function duplicateUser {
 y
 EOF
     
-    grep --include "*.rc" --include "*.desktop" --include "*.sh" --include "*.ini" -m1 -rnHE "${user_to_copy}" "/home/${new_user}/${file}" | awk -F ":" -v _from_user="${user_to_copy}" -v _to_user="${new_user}" '{system("sed -i -e \"s/\\/"_from_user"/\\/"_to_user"/g\" "$1)}';
+    grep --include "*.rc" --include ".*" \
+         --include "*.desktop" --include "*.sh" --include "*.ini" \
+         -m1 -rnHE "${user_to_copy}" "/home/${new_user}/${file}" | awk -F ":" -v _from_user="${user_to_copy}" -v _to_user="${new_user}" '{system("sed -i -e \"s/\\/"_from_user"/\\/"_to_user"/g\" "$1)}';
   done;
   
   sudo gpasswd -a $new_user docker;
