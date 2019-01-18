70ded59393c98e0e36e22e849b04484bf9eec3c4
poxstone <poxstone@gmail.com>
2019-01-18 07:05:50 -0500

    fix docker compose

--- desktostart/centos7-cloud/i.sh
+++ desktostart/centos7-cloud/i.sh
@@ -456,7 +456,11 @@ function dockerTools {
   # activate docker daemon
   sudo systemctl start docker;
   # docker compose
-  sudo pip install docker-compose;
+  #sudo pip install docker-compose;
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+  sudo chmod +x /usr/local/bin/docker-compose;
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose;
+
 }
 
 # Android dev
