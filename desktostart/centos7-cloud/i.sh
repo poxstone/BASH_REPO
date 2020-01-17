b5e1d710e2ca74527bf34c2c47d1c8b371e07c7f
poxstone <poxstone@gmail.com>
2020-01-17 07:13:55 -0500

    Update i.sh

--- desktostart/centos7-cloud/i.sh
+++ desktostart/centos7-cloud/i.sh
@@ -114,7 +114,7 @@ function mountDisk {
 # Install tools
 function mainTools {
   updateSystem;
-  sudo yum install vim nano tmux htop iotop lynx nmap whois tcpdump iotop bind-utils wget -y;
+  sudo yum install vim nano tmux htop iotop lynx nmap whois tcpdump iotop bind-utils wget whois -y;
 }
 
 # Dev tools
