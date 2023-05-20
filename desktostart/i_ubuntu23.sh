1a401310debf8a85bba4e589834aa8c4eb50df74
poxstone <poxstone@gmail.com>
2023-05-19 21:46:58 -0500

    Update i_ubuntu23.sh

--- desktostart/i_ubuntu23.sh
+++ desktostart/i_ubuntu23.sh
@@ -161,6 +161,12 @@ function dockerTools {
   sudo chmod +x /usr/local/bin/docker-compose;
 }
 
+function minikubeInstall {
+  # https://minikube.sigs.k8s.io/docs/start/
+  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64;
+  sudo install minikube-linux-amd64 /usr/local/bin/minikube -y;
+}
+
 # Android dev
 #function javaAndroid {
 #  #sudo fastboot oem get_unlock_data
