ed73e0591dfcffefe9e8eba0b000af8115e61328
poxstone <poxstone@gmail.com>
2023-08-13 13:29:11 -0500

    Update i_ubuntu23.sh

--- desktostart/i_ubuntu23.sh
+++ desktostart/i_ubuntu23.sh
@@ -279,21 +279,6 @@ function nvidiaDrivers {
   nvcc --version;
 }
 
-function kali {
-  sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean;
-  sudo apt-get install -y wget gnupg dirmngr;
-  wget -q -O - https://archive.kali.org/archive-key.asc | sudo gpg --import;
-  sudo echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list;
-  sudo gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -;
-  sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean;
-  sudo apt --fix-broken install;
-  # solve someones problems
-  sudo apt-get install -y kali-linux-default;
-  touch kali_update.sh;
-  echo "apt-get update -y && apt-get full-upgrade -y && apt-get dist-upgrade -y && apt autoremove -y && apt autoclean" > kali_update.sh;
-  chmod +x kali_update.sh;
-  
-}
 
 function brewInstall {
   sudo apt-get update -y;
